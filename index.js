6e9ebe6ac52159529592566152a2d9cb1c22c3fe
Ximena-21 <ximenanzola.21@gmail.com>
2022-08-29 21:48:58 -0400

    inicializar paquete npm, pruebas de lecturas de archivos, validacion de ruta

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 module.exports = () => {
   // ...
 };
+
+//traer modulo de fs, con el require.js , instalado por defecto
+  //todos los metodos vienen de forma asincrona
+const fs = require('fs')
+
+function readFile (path, cb) {
+  //funcion del fs para leer archivos
+  fs.readFile(path, (err, data)=>{
+    console.log(data.toString())
+  })
+
+
+}
+
+//dirname para decrile que el archivo lo tenemos en el directorio
+readFile(__dirname + '/archivo.md')
+
+
+
+// .............. validar ruta de archivo absoluta o relativa ............
+
+const path = require('path')
+
+
+
+//validar si la ruta es absoluta?, 
+  //path.isAbsolute()método determina si path es una ruta absoluta.
+  //devuelve un true si es absolute
+if(path.isAbsolute(path)) {
+  //...
+}
+
+//si no lo es convertirla a absoluta con el .resolve
+const absolutePath = path.resolve('relative path')
+
+
+
+
+
+//leer la extension de un archivo apartir de su ruta .extname
+
+
+
