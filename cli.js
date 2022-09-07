49d964fe742e9c44fd61d2c23902a5d6ac25b168
Ximena-21 <ximenanzola.21@gmail.com>
2022-09-07 18:50:52 -0400

    se termino el CLI

--- cli.js
+++ cli.js
@@ -1,7 +1,27 @@
 #! /usr/bin/env node
 
-// const funciones = require('./index')
-import { temporal, getFiles } from "./index";
+import { mdLinks } from "./index.js"
+import { validateLink, statsLinks, statsValidatelinks } from "./src/validateLinks.js"
 
-temporal()
+const argv = process.argv //matriz con propiedades de la linea de comandos
+
+mdLinks(argv[2], argv)
+    .then((arrObjectLinks) => {
+        if (argv.length < 3) {
+            return arrObjectLinks
+        }
+        if (argv.includes('--validate') || argv.includes('--v')) {
+            return validateLink(arrObjectLinks)
+        }
+        if (argv.includes('--stats') || argv.includes('--s')) {
+            return statsLinks(arrObjectLinks)
+        }
+        if ((argv.includes('--validate') || argv.includes('--v')) && (argv.includes('--stats') || argv.includes('--s'))) {
+            return statsValidatelinks(arrObjectLinks)
+        }
+        console.log('envia una opcion valida')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
